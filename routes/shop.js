56aa89724878913db56109eb98e781cc9e7c3a2c
mathiamry <mathiamry@gmail.com>
2021-01-18 21:35:50 +0000

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const rootDir = require('../util/path');
+const path = require('path');
+const router = express.Router();
+
+const adminData = require('./admin')
+
+
+router.get('/',(req, res, next)=>{
+    const products = adminData.products;
+    res.render('shop',{ prods: products, docTitle:"Shop"});
+    // console.l og(adminData.products);
+    // res.sendFile(path.join(rootDir,'views','shop.html'));
+})
+
+module.exports = router;

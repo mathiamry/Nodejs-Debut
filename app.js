085e305e93085386fb2e9a9b3ae32e9a55ccb2ee
mathiamry <mathiamry@gmail.com>
2021-01-19 01:13:58 +0000

    adding controllers

--- app.js
+++ app.js
@@ -3,21 +3,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const pageNotFoundController = require('./controllers/404.js');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const router = express.Router();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
-app.use(shopRoutes);
-
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(pageNotFoundController.pageNotFound);
 
 app.listen(3000);
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,23 +2,9 @@ const express= require('express');
 const rootDir = require('../util/path');
 const path = require('path');
 const router = express.Router();
+const productsController = require('../controllers/products');
 
-const adminData = require('./admin')
 
-
-router.get('/',(req, res, next)=>{
-    const products = adminData.products;
-    res.render('shop',
-    {   prods: products, 
-        pageTitle:"Shop",
-         path: '/', 
-         hasProducts: products.length>0,
-         activeShop: true,
-         productCSS: true,
-        
-        });
-    // console.l og(adminData.products);
-    // res.sendFile(path.join(rootDir,'views','shop.html'));
-})
+router.get('/',productsController.getProducts)
 
 module.exports = router;
